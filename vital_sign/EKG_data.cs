fb96766fa105fff21ba54f62360e1818c3b7e45f
esztikiraly <kiraly.eszter0913@gmail.com>
2022-11-01 18:41:16 +0100

    initial commit

---
+++ vital_sign/EKG_data.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MindFusion.Charting;
+
+
+namespace vital_sign
+{
+    public class EKG_data : Series
+
+    {
+        public EKG_data(DateTime start, DateTime minDate, DateTime maxDate)
+        {
+            this.start = start;
+
+            this.minDate = minDate;
+            this.maxDate = maxDate;
+
+            dateTimeFormat = DateTimeFormat.ShortTime;
+            customDateTimeFormat = "";
+            labelInterval = 10;
+
+            dates = new List<long>();
+            values = new List<double>();
+        }
+
+        public int Size
+        {
+            get { return values.Count; }
+        }
+
+        public int Dimensions
+        {
+            get { return 2; }
+        }
+
+        public string Title
+        {
+            get;set;
+        }
+
+        public LabelKinds SupportedLabels
+        {
+            get { return labelKinds; }
+            set { labelKinds = value; }
+        }
+
+        public event EventHandler DataChanged;
+
+        protected virtual void OnDataChanged()
+        {
+            if (DataChanged!= null)
+                DataChanged(this, EventArgs.Empty);
+        }
+
+        public string GetLabel(int index, LabelKinds kind)
+        {
+            if (kind==LabelKinds.XAxisLabel)
+            {
+                if (index<values.Count && index %labelInterval==0)
+                {
+                    DateTime dateTime = new DateTime(dates[index]);
+
+                    SortedList dateTimeFormats = new SortedList(9);
+                    dateTimeFormats.Add("d", DateTimeFormat.ShortDate);
+                    dateTimeFormats.Add("D", DateTimeFormat.LongDate);
+                    dateTimeFormats.Add("t", DateTimeFormat.ShortTime);
+                    dateTimeFormats.Add("T", DateTimeFormat.LongTime);
+                    dateTimeFormats.Add("M", DateTimeFormat.MonthDateTime);
+                    dateTimeFormats.Add("Y", DateTimeFormat.YearDateTime);
+                    dateTimeFormats.Add("f", DateTimeFormat.FullDateTime);
+                    dateTimeFormats.Add("*", DateTimeFormat.CustomDateTime);
+                    dateTimeFormats.Add("", DateTimeFormat.None);
+
+                    string format = customDateTimeFormat;
+
+                    if (dateTimeFormat != DateTimeFormat.None && 
+                        dateTimeFormat != DateTimeFormat.CustomDateTime)
+                    {
+                        int fIndex = dateTimeFormats.IndexOfValue(dateTimeFormat);
+                        format = dateTimeFormats.GetKey(fIndex).ToString();
+                    }
+
+                    return dateTime.ToString(format);
+                }
+               
+            }
+            return string.Empty;
+        }
+
+        public double GetValue(int index, int dimension)
+        {
+            if (dimension == 0)
+            {
+                if (index < dates.Count && index>=0)
+                {
+                    long currValue = dates[index];
+                    
+                    var p = (currValue-(double)minDate.Ticks)/((double)maxDate.Ticks-(double)minDate.Ticks);
+
+                    return minValue + ((maxValue - minValue) * p);
+                }
+            }
+            if (dimension==1) return values[index];
+            return 0;
+            
+        }
+
+        public bool IsEmphasized(int index)
+        {
+            return false;
+        }
+
+        public bool IsSorted(int dimension)
+        {
+            return dimension == 0;
+        }
+
+        public void addValue(double value)
+        {
+            this.values.Add(value);
+            long currTime = DateTime.Now.Ticks;
+            dates.Add(currTime);
+        }
+
+        public DateTimeFormat DateTimeFormat
+        {
+            get { return dateTimeFormat; }
+            set
+            {
+                if (dateTimeFormat == value)
+                    return;
+                dateTimeFormat = value;
+                OnDataChanged();
+            }
+        }
+
+        public int LabelInterval
+        {
+            get { return labelInterval; }
+            set 
+            {
+                if (labelInterval == value)
+                    return;
+                labelInterval = value;
+                OnDataChanged();
+            }
+        }
+
+
+        //start of series
+        DateTime start;
+
+        //values (Y and X data)
+        List<double> values;
+        List<long> dates;
+
+        //start and end points of the x axis
+        private DateTime minDate;
+        private DateTime maxDate;
+
+        //start and end points of the y axis
+        private double minValue = 0;
+        private double maxValue = 1;
+
+        //labels
+        private int labelInterval;
+        private DateTimeFormat dateTimeFormat;
+        private string customDateTimeFormat;
+        private LabelKinds labelKinds;
+    }
+}
--- vital_sign/Form1.Designer.cs
+++ vital_sign/Form1.Designer.cs
@@ -28,17 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.button1 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.lineChart1 = new MindFusion.Charting.WinForms.LineChart();
             this.SuspendLayout();
             // 
             // button1
@@ -89,38 +84,32 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // chart1
+            // lineChart1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(93, 12);
-            this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(874, 271);
-            this.chart1.TabIndex = 8;
-            this.chart1.Text = "chart1";
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(93, 305);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(874, 169);
-            this.richTextBox1.TabIndex = 9;
-            this.richTextBox1.Text = "";
+            this.lineChart1.LegendTitle = "Legend";
+            this.lineChart1.Location = new System.Drawing.Point(117, 12);
+            this.lineChart1.Name = "lineChart1";
+            this.lineChart1.Padding = new System.Windows.Forms.Padding(5);
+            this.lineChart1.ShowLegend = true;
+            this.lineChart1.Size = new System.Drawing.Size(850, 462);
+            this.lineChart1.SubtitleFontName = null;
+            this.lineChart1.SubtitleFontSize = null;
+            this.lineChart1.SubtitleFontStyle = null;
+            this.lineChart1.TabIndex = 8;
+            this.lineChart1.Text = "lineChart1";
+            this.lineChart1.Theme.UniformSeriesFill = new MindFusion.Drawing.SolidBrush("#FF90EE90");
+            this.lineChart1.Theme.UniformSeriesStroke = new MindFusion.Drawing.SolidBrush("#FF000000");
+            this.lineChart1.Theme.UniformSeriesStrokeThickness = 2D;
+            this.lineChart1.TitleFontName = null;
+            this.lineChart1.TitleFontSize = null;
+            this.lineChart1.TitleFontStyle = null;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(979, 486);
-            this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.lineChart1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
@@ -129,7 +118,6 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -142,8 +130,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private MindFusion.Charting.WinForms.LineChart lineChart1;
     }
 }
 
